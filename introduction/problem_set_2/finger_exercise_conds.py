12dc4c32deca24194f5648e414dc646d02a29009
TakeThatForDataFiz <johnlunz8@gmail.com>
2025-07-24 20:35:17 -0400

    refactored get_special_odd to improve performance

--- introduction/problem_set_2/finger_exercise_conds.py
+++ introduction/problem_set_2/finger_exercise_conds.py
@@ -3,17 +3,19 @@ finger_exercise_conds.py -- script returning largest of 3 numbers if odd,
                                 otherwise return smallest number
 """
 
+def is_even(num):
+    return num % 2 == 0
+
 def get_special_odd(num_1: int, num_2: int, num_3: int):
-    if num_1 % 2 == 0 and num_2 % 2 == 0 and num_3 % 2 == 0:
-        return min(num_1, num_2, num_3)
-    else:
         odds = []
-        if num_1 % 2 != 0:
+        if not is_even(num_1):
             odds.append(num_1)
-        if num_2 % 2 != 0:
+        if not is_even(num_2):
             odds.append(num_2)
-        if num_3 % 2 != 0:
+        if not is_even(num_3):
             odds.append(num_3)
         if not odds:
-            raise ValueError(f"Values - 1){num_1} 2){num_2} 3){num_3} passed are invalid")
-        return max(odds)
+            return min(num_1, num_2, num_3)
+        else:
+            return max(odds)
+        
--- introduction/problem_set_2/tests/test_finger_exercise_conds.py
+++ introduction/problem_set_2/tests/test_finger_exercise_conds.py
@@ -1,7 +1,10 @@
 import pytest
 import sys
 sys.path.append('../')
-from finger_exercise_conds import get_special_odd
+from finger_exercise_conds import get_special_odd, is_even
+
+def get_special_odd_wrapper(num):
+    get_special_odd(num_1=num, num_2=num, num_3=num)
 
 @pytest.mark.parametrize(
         "num_1, num_2, num_3, exp_value",
@@ -14,6 +17,28 @@ from finger_exercise_conds import get_special_odd
 )
 def test_get_special_odd(num_1, num_2, num_3, exp_value):
     get_special_odd(num_1=num_1, num_2=num_2, num_3=num_3) == exp_value
+
+@pytest.mark.parametrize(
+        "num, exp",
+        [
+            (1, False),
+            (2, True),
+            (0, True),
+        ]
+)
+def test_is_even(num, exp):
+    assert is_even(num) == exp
+
+@pytest.mark.parametrize(
+        "num, func, excp",
+        [
+            ("bad", is_even, TypeError),
+            ("bad", get_special_odd_wrapper, TypeError),
+        ]
+)
+def test_excp_raises(num, func, excp):
+    with pytest.raises(excp):
+        func(num)
 
 if __name__ == "__main__":
     pytest.main()
