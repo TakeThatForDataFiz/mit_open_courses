636ca75f3877c08be4f790e29a80a74887ee91cc
TakeThatForDataFiz <johnlunz8@gmail.com>
2025-07-29 22:25:54 -0400

    finger exercise to return root and power for a given integer if it exists

---
+++ introduction/problem_set_3/finger_exer_powers.py
@@ -0,0 +1,20 @@
+'''
+finger_exer_powers.py -- script that returns root & power based on integer entered
+by user -- 1 < pwr < 6
+'''
+
+
+
+
+def get_root_and_power(exp: int):
+    for guess in range(exp):
+        for power in range(2, 6):
+            if guess**power == exp:
+                return [guess, power]
+    print("No matches found")
+    return None 
+
+
+
+if __name__ == "__main__":
+    print(get_root_and_power(6))
---
+++ introduction/problem_set_3/tests/test_finger_exer_powers.py
@@ -0,0 +1,25 @@
+"""
+test_finger_exer_powers.py
+    -- test powers exercise script
+"""
+import pytest
+import sys
+
+sys.path.append("../")
+import finger_exer_powers as p
+
+
+
+@pytest.mark.parametrize(
+    "input, exp_val",
+    [
+    (25, [5, 2]),
+    (8, [2, 3]),
+    (11, None),
+]
+)
+def test_finger_power_exer(input, exp_val):
+   assert p.get_root_and_power(input) == exp_val 
+
+if __name__ == "__main__":
+    pytest.main()
